43eb52a900c52868163c6cf6082ebebd7b8a62c6
peanghs <peanghs@naver.com>
2019-01-12 17:13:08 +0900

    Add files via upload

--- titanic_basic.py
+++ titanic_basic.py
@@ -3,6 +3,7 @@ import csv
 import numpy as np
 from sklearn.preprocessing import MinMaxScaler
 
+
 ''' 
 0 PassengerId - type should be integers
 1 Survived - Survived or Not
@@ -33,7 +34,7 @@ train_label = []
 for record in train_db:
     if record[0] != 'PassengerId':
         ID = float(record[0])
-        Survived = float(record[1])
+        Survived = int(record[1])
         PClass = record[2]
         if PClass == '':
             PClass = 0.0
@@ -80,8 +81,11 @@ for record in train_db:
         train_label.append(label_array_info)
 
 # #  학습 자료 라벨 원핫 처리
-# train_label = tf.one_hot(train_label, depth=2).eval()
-# train_label = tf.reshape(train_label, shape=[-1, 2]).eval()
+# train_label = tf.one_hot(train_label, depth=2)
+# print(train_label)
+# train_label = tf.reshape(train_label, shape=[-1, 2])
+# print(train_label)
+
 train_set = np.array(train_set)
 train_set = MinMaxScaler().fit_transform(train_set)
 print('-----Train Set-----')
@@ -144,10 +148,12 @@ for record in test_db:
 
 for record in test_label_db:
     if record[0] != 'PassengerId':
-        Survived = float(record[1])
+        Survived = int(record[1])
         test_label_array_info = [Survived]
         test_label.append(test_label_array_info)
 
+# test_label = tf.one_hot(test_label, depth=2)
+# test_label = tf.reshape(test_label, shape=[-1, 2])
 test_set = np.array(test_set)
 test_set = MinMaxScaler().fit_transform(test_set)
 print('-----Test Set-----')
@@ -157,31 +163,35 @@ print(test_label)
 print('실험 자료 길이:', '%d' % (len(test_set)))
 
 X = tf.placeholder(tf.float32, [None, 7])
-Y = tf.placeholder(tf.float32, [None, 1])
-b1 = tf.Variable(tf.random_normal([1]))
-b2 = tf.Variable(tf.random_normal([1]))
-b3 = tf.Variable(tf.random_normal([1]))
+Y = tf.placeholder(tf.int32, [None, 1])
+Y_OneHot = tf.one_hot(Y, depth=2)
+Y_OneHot = tf.reshape(Y_OneHot, shape=[-1, 2])
+
+b1 = tf.Variable(tf.random_normal([5]))
+b2 = tf.Variable(tf.random_normal([4]))
+b3 = tf.Variable(tf.random_normal([2]))
 
 W1 = tf.Variable(tf.random_normal([7, 5], stddev=0.01))
 L1 = tf.nn.relu(tf.matmul(X, W1) + b1)
 
-W2 = tf.Variable(tf.random_normal([5, 3], stddev=0.01))
+W2 = tf.Variable(tf.random_normal([5, 4], stddev=0.01))
 L2 = tf.nn.relu(tf.matmul(L1, W2) + b2)
 
-W3 = tf.Variable(tf.random_normal([3, 1], stddev=0.01))
+W3 = tf.Variable(tf.random_normal([4, 2], stddev=0.01))
 model = tf.matmul(L2, W3) + b3
 
-cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=model, labels=Y))
-optimizer = tf.train.AdamOptimizer(0.001).minimize(cost)
+cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=model, labels=Y_OneHot))
+optimizer = tf.train.AdamOptimizer(0.01).minimize(cost)
 
-is_correct = tf.equal(tf.argmax(model, 1), tf.argmax(Y, 1))
+prediction = tf.argmax(model, 1)
+is_correct = tf.equal(tf.argmax(model, 1), tf.argmax(Y_OneHot, 1))
 accuracy = tf.reduce_mean(tf.cast(is_correct, tf.float32))
 
 init = tf.global_variables_initializer()
 sess = tf.Session()
 sess.run(init)
 
-batch_size = 30
+batch_size = 1
 total_batch = int(len(train_set) / batch_size)
 print('토탈 배치:', total_batch)
 
@@ -189,6 +199,7 @@ for epoch in range(15):
     total_cost = 0
 
     for i in range(total_batch):
+        # print('---------', i, '---------------')
         # x_batch, y_batch = sess.run([train_set, train_label])
         # _, cost_val = sess.run([optimizer, cost], feed_dict={X: x_batch, Y: y_batch})
         _, cost_val = sess.run([optimizer, cost], feed_dict={X: train_set, Y: train_label})
@@ -197,4 +208,6 @@ for epoch in range(15):
     print('Epoch:', '%d' % (epoch + 1), 'Avg. cost =', '{:.3f}'.format(total_cost / total_batch))
 
 print('최적화 완료!')
-print('정확도:', sess.run(accuracy, feed_dict={X: test_set, Y: test_label}))
+p, a = sess.run([prediction, accuracy], feed_dict={X: test_set, Y: test_label})
+print('정확도:', a)
+print ('예측 : ', p)
